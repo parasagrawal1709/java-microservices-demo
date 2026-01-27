783e6b393e971728b395b24f85e680577f1bff21
parasagrawal17 <parrrasagrawal.1709@gmail.com>
2026-01-27 12:06:57 +0530

    Update Product-Service

--- product-service/src/main/java/com/demo/product/controller/ProductController.java
+++ product-service/src/main/java/com/demo/product/controller/ProductController.java
@@ -9,6 +9,6 @@ public class ProductController {
 
     @GetMapping
     public List<String> getProducts() {
-        return List.of("Laptop", "Phone", "Ipad", "iMac", "Airpods");
+        return List.of("Laptop", "Phone", "Ipad", "iMac", "Airpod", "xyz");
     }
 }
